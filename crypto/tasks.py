91c0a9f9cced6cbef1e5734a80467e3cf2b8f5eb
fufic123 <semenovmark07@gmail.com>
2024-01-28 05:37:38 +0200

    task

--- crypto/tasks.py
+++ crypto/tasks.py
@@ -12,41 +12,44 @@ def binance_price():
 
 
     for coin in coinList:
-        try:
-            obj = Crypto.objects.get(symbol=coin)
-        except Crypto.DoesNotExist:
+        crypto_objects = Crypto.objects.filter(symbol=coin)
+
+        if not crypto_objects.exists():
             print(coin)
-            continue  # Пропустить итерацию, если монета не найдена
+            continue  # Skip iteration if no matching coin is found
 
-        binance_url = f"https://api.binance.com/api/v3/ticker/24hr?symbol={obj.symbol}USDT"
-        response = requests.get(binance_url)
-        if response.status_code == 200:
-            data = response.json()  # Parse JSON response
-            price = data.get('lastPrice', '0')  # Get the 'price' field from the JSON data
-            price = Decimal(price)
-            obj.price = price        
-        else:
-            url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-            params = {
-                'start': '1',      # Start index of the cryptocurrency data
-                'limit': '1000',    # Number of cryptocurrencies to retrieve
-                'convert': 'USD'   # Currency to convert the prices to
-            }
-            headers = {
-                'Accepts': 'application/json',
-                'X-CMC_PRO_API_KEY': CoinMarketCup
-            }
-            response = requests.get(url, params=params, headers=headers)
+        for obj in crypto_objects:
+            binance_url = f"https://api.binance.com/api/v3/ticker/24hr?symbol={obj.symbol}USDT"
+            response = requests.get(binance_url)
 
-            data = response.json()
-            
-            if 'data' in data:
-                for cryptocurrency in data['data']:
-                    symbol = cryptocurrency['symbol']
-                    if symbol in coinList:  # Filter specific cryptocurrencies
-                        obj.price = cryptocurrency['quote']['USD']['price']
-        try:
-            obj.save()
-        except Exception as e:
-            # Handle any additional errors when saving the object
-            print(f"Error saving object: {e}")
+            if response.status_code == 200:
+                data = response.json()
+                price = data.get('lastPrice', '0')
+                price = Decimal(price)
+                obj.price = price
+            else:
+                url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+                params = {
+                    'start': '1',
+                    'limit': '1000',
+                    'convert': 'USD'
+                }
+                headers = {
+                    'Accepts': 'application/json',
+                    'X-CMC_PRO_API_KEY': CoinMarketCup
+                }
+                response = requests.get(url, params=params, headers=headers)
+
+                data = response.json()
+
+                if 'data' in data:
+                    for cryptocurrency in data['data']:
+                        symbol = cryptocurrency['symbol']
+                        if symbol == coin:
+                            obj.price = cryptocurrency['quote']['USD']['price']
+
+            try:
+                obj.save()
+            except Exception as e:
+                # Handle any additional errors when saving the object
+                print(f"Error saving object: {e}")
