7aae9bd1bd05827c4cccc6e8baa741e392d2dd76
fufic123 <semenovmark07@gmail.com>
2024-01-29 05:30:03 +0200

    bank in home.html

--- crypto/views.py
+++ crypto/views.py
@@ -19,10 +19,6 @@ def home(request):
     deposit = DepositPayment.objects.filter(is_available=True)
     settings = DepositSettings.objects.get(title="Настройки депозита")
     banks = Bank.objects.filter(is_available=True)
-    
-    for dep in deposit:
-        dep.crypto.price -= dep.crypto.price * Decimal(0.025)
-        dep.crypto.save()
 
     try:
         obj = Crypto.objects.get(symbol="BTC",is_available=True)
