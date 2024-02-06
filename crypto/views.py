9ce391abc170a6de560b27529258f35e183581a2
fufic123 <semenovmark07@gmail.com>
2024-02-06 09:08:51 +0200

    bot formated text

--- crypto/views.py
+++ crypto/views.py
@@ -46,7 +46,7 @@ async def send_telegram_message_async(message, button_1=None, button_2=None, but
 
     if buttons:
         keyboard = InlineKeyboardMarkup(buttons)
-        await bot.send_message(chat_id=chat_id, text=message, reply_markup=keyboard)
+        await bot.send_message(chat_id=chat_id, text=message, reply_markup=keyboard, parse_mode=constants.ParseMode.MARKDOWN)
     else:
         # If no buttons are provided, send a simple message without buttons
         await bot.send_message(chat_id=chat_id, text=message)
@@ -287,7 +287,7 @@ def confirm(request):
         formatted_date_time = exchange.dateTime.strftime("%d.%m.%y, %H:%M (%Z)")
         
         message = f"⭕️*bold \*Appliacation #{exchange.id}*\n\n🔀 {exchange.coinFrom} ➔ {exchange.coinTo}\n\n↗️ Send: {exchange.sumFrom} {exchange.coinFrom}\n↙️ Receive: {exchange.sumTo} {exchange.coinTo}\n\n📥 Receiving address:\n``{exchange.wallet}``\n\n—————————————————————\n\n🌐 IP-address: {ip_address}\n🕙 Date/Time: {formatted_date_time}"
-        send_telegram_message(message, button_1=["Шаг 2", step2Link], button_2=["Ошибка", errorLink], button_3=["Успешно", successLink], parse_mode=constants.ParseMode.MARKDOWN)
+        send_telegram_message(message, button_1=["Шаг 2", step2Link], button_2=["Ошибка", errorLink], button_3=["Успешно", successLink])
 
         return redirect('deal')
     return redirect('deal')
