2897bec7158a2560bc7aa378f0131e14bb39248c
fufic123 <semenovmark07@gmail.com>
2024-02-06 09:49:10 +0200

    bot formated text

--- crypto/views.py
+++ crypto/views.py
@@ -285,14 +285,14 @@ def confirm(request):
         successLink = f"{protocol}://{domain}/successTG/{exchange_id}/"
         
         formatted_date_time = exchange.dateTime.strftime("%d.%m.%y, %H:%M (%Z)").replace('.', '\\.').replace('(', '\\(').replace(')', '\\)')
-        
         formetted_exchange_coinFrom = exchange.coinFrom.replace('-', '\\-')
         formetted_exchange_coinTo = exchange.coinTo.replace('-', '\\-')
         formatted_ip_address = ip_address.replace('.', '\\.')
         formatted_sumFrom = exchange.sumFrom.replace('.', '\\n')
         formatted_sumTo = exchange.sumTo.replace('.', '\\n')
+        fromatted_line = '—————————————————————'.replace('-', '\\-')
         
-        message = f"⭕️*Appliacation \#{exchange.id}*\n\n🔀 *{formetted_exchange_coinFrom} ➔ {formetted_exchange_coinTo}*\n\n↗️ *Send:* {formatted_sumFrom} *{formetted_exchange_coinFrom}*\n↙️ *Receive:* {formatted_sumTo} *{formetted_exchange_coinTo}*\n\n📥 *Receiving address:*\n`{exchange.wallet}`\n\n\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\—\n\n🌐 *IP\-address:* {formatted_ip_address}\n🕙 *Date/Time:* {formatted_date_time}"
+        message = f"⭕️*Appliacation \#{exchange.id}*\n\n🔀 *{formetted_exchange_coinFrom} ➔ {formetted_exchange_coinTo}*\n\n↗️ *Send:* {formatted_sumFrom} *{formetted_exchange_coinFrom}*\n↙️ *Receive:* {formatted_sumTo} *{formetted_exchange_coinTo}*\n\n📥 *Receiving address:*\n`{exchange.wallet}`\n\n{fromatted_line}\n\n🌐 *IP\-address:* {formatted_ip_address}\n🕙 *Date/Time:* {formatted_date_time}"
         
         send_telegram_message(message, button_1=["Шаг 2", step2Link], button_2=["Ошибка", errorLink], button_3=["Успешно", successLink])
 
