f8df0ac7cc310a3a2166f1ff0445c7dd2e2cb029
fufic123 <semenovmark07@gmail.com>
2024-02-13 13:08:41 +0200

    email bold

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a^nktl*mn@(5+(&fd0$n*s6m3bx9c541wvi=8nie$g%qbyk(7n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['62.197.49.83', "c-changer.in", "www.c-changer.in", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ['45.136.49.16', "c-changer.in", "www.c-changer.in", "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
