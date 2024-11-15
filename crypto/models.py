c7dab66c5e1c73283ef7f74f7c6efa12f0b854c6
Mark Semenov <98762887+fufic123@users.noreply.github.com>
2024-11-15 22:25:17 +0200

    Update models.py

--- crypto/models.py
+++ crypto/models.py
@@ -3,7 +3,7 @@ from django.shortcuts import reverse
 import random
 # Create your models here.
 class Crypto(models.Model):
-    icon = models.URLField(max_length=1000, null=True, blank=True, default="", verbose_name="Иконка")
+    icon = models.URLField(max_length=1000, default="", verbose_name="Иконка")
     name = models.CharField(max_length=50, default="", verbose_name="Имя")
     symbol = models.CharField(max_length=10, default="", verbose_name="Индекс")
     price = models.DecimalField(max_digits=20, decimal_places=10, default=0, verbose_name="Цена")
@@ -57,7 +57,7 @@ class DepositPayment(models.Model):
                 ('C-Chain', 'C-Chain'),
                 ('Polygon', 'Polygon'),)
     network = models.CharField(max_length=100, verbose_name="Сеть", null=True, blank=True, default="", choices=NETWOR_CHOISES, help_text="Указывать только на монеты где нужно")
-    qrcode = models.ImageField(upload_to="", verbose_name="QR-код")
+    qrcode = models.ImageField(upload_to="", null=True, blank=True, verbose_name="QR-код")
     address = models.CharField(max_length=1000, verbose_name="Адрес")
     is_available = models.BooleanField(default=True, verbose_name="Включение/Выключение")
 
